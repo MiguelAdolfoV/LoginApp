c90d07cb6243553f1ee13bc62ef904f67ffa0f51
Ninis <MiguelAdolfoV@outlook.com>
2025-02-12 19:54:13 -0600

    Funcional

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,33 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  username: string = '';
+  password: string = '';
+  isValid: boolean = false;
 
-  constructor() {}
+  constructor(private alertCtrl: AlertController) {}
 
+  validateInputs(inputType: string) {
+    if (inputType === 'username') {
+      this.username = this.username.replace(/\s/g, '').toLowerCase(); 
+    } else if (inputType === 'password') {
+      this.password = this.password.replace(/\s/g, '');
+    }
+
+    this.isValid = this.username.length > 0 && this.password.length > 0;
+  }
+
+  async showModal() {
+    const alert = await this.alertCtrl.create({
+      header: 'Información',
+      message: '',
+      buttons: ['Cerrar']
+    });
+  
+    alert.message = `Username: ${this.username}
+                     Password:${this.password}`;
+  
+    await alert.present();
+  }
+  
 }
