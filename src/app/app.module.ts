65e1aab3970a144fa37ed19cb04d414cb8ee326c
Ninis <MiguelAdolfoV@outlook.com>
2025-02-18 11:52:19 -0600

    [Add] - Integrate Firebase into the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+
+import { environment } from '../environments/environment';
+
+
 @NgModule({
   declarations: [AppComponent, LoadingInitComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideDatabase(() => getDatabase()),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,22 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBWuOI8YeGwdXJN05riNFIMskTYs3235Nk",
-  authDomain: "loginapp-bbece.firebaseapp.com",
-  databaseURL: "https://loginapp-bbece-default-rtdb.firebaseio.com",
-  projectId: "loginapp-bbece",
-  storageBucket: "loginapp-bbece.firebasestorage.app",
-  messagingSenderId: "856404781082",
-  appId: "1:856404781082:web:e3c1de701be28507ddc9a3",
-  measurementId: "G-CQW596MSW0"
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBWuOI8YeGwdXJN05riNFIMskTYs3235Nk",
+    authDomain: "loginapp-bbece.firebaseapp.com",
+    databaseURL: "https://loginapp-bbece-default-rtdb.firebaseio.com",
+    projectId: "loginapp-bbece",
+    storageBucket: "loginapp-bbece.firebasestorage.app",
+    messagingSenderId: "856404781082",
+    appId: "1:856404781082:web:e3c1de701be28507ddc9a3",
+    measurementId: "G-CQW596MSW0"
+  }
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
